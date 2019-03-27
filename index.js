a8d46f550bf118eb55b1c33fb2b3f295bd1f35f0
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-27 14:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
 
-  if (string === lowercase)
+  if (string === toLowerCase)
   return ("I can't hear you!")
-}
+
+else (string === toUpperCase)
+return ("I love you, Grandma")}
